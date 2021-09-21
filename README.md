08ee80d52c118d0cea5d5e16e07d8a9952ee186f
carlosp <carlosmpr0508@gmail.com>
2021-09-21 09:38:26 -0400

    Func added

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Python File manipulation
 
-Python program that create template automatically, ideal for emails, resume , message and 
+Python program that creates template automatically with python open function , ideal for emails, resume, message and 
 cover letters, contact list, etc.
+
+>The open() function opens a file, and returns it as a file object.
+## Code
+
+1. Assign your values to the data array:
+
+        data = [{'name': 'Carlos', 'company': 'Google', 'role': 'Front End'},
+                {'name': 'Jhon', 'company': 'Microsoft', 'role': 'Full
+
+2. Change the cover letter information .
+3. Run the code.
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # How to open read and write files in python
 
-with open('names.txt','+') as textFile:
-    textFile.write("Hello from the computer")
-    print(textFile.read())
+data = [{'name': 'Carlos', 'company': 'Google', 'role': 'Front End'}, {'name': 'Jhon', 'company': 'Microsoft', 'role': 'Full-Stack'}, {'name': 'Doe', 'company': 'Amazon', 'role': 'Backend'}]
+
+with open('coverletter.txt') as letter:
+    letter_content = letter.read()
+    for name in data:
+        new_letter = letter_content.replace('[name]', name['name'])
+        new_letter = new_letter.replace('[company]', name['company'])
+        new_letter = new_letter.replace('[role]', name['role'])
+        with open(f'{name}_letter.text', 'w+') as new:
+            new.write(new_letter)
